11ccc59e311eeda05cd653703ec68b9db5ccf537
belven <belven000@gmail.com>
2014-08-01 11:33:22 +0100

    Added manager for bukkit

---
+++ src/resources/ResourcesManager.java
@@ -0,0 +1,21 @@
+package resources;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class ResourcesManager extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+    }
+
+    @Override
+    public void onDisable()
+    {
+        getLogger().info("Goodbye world!");
+        this.saveConfig();
+    }
+
+}
