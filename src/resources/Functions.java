65a14be7f44dc7e827bfe402b464d406891f9824
killer520 <elias.heyndrickx@gmail.com>
2014-08-02 19:38:42 +0200

    Resources

--- src/resources/Functions.java
+++ src/resources/Functions.java
@@ -1,8 +1,6 @@
 package resources;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 import org.bukkit.Bukkit;
@@ -10,61 +8,13 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Damageable;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Slime;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionType;
-import org.bukkit.util.Vector;
 
-public class Functions
-{
-    public static Location move(Location loc, Vector offset)
-    {
-        return move(loc, offset.getX(), offset.getY(), offset.getZ());
-    }
-
-    public static Location move(Location loc, double dx, double dy, double dz)
-    {
-        Vector off = rotate(loc.getYaw(), loc.getPitch(), dx, dy, dz);
-        double x = loc.getX() + off.getX();
-        double y = loc.getY() + off.getY();
-        double z = loc.getZ() + off.getZ();
-        return new Location(loc.getWorld(), x, y, z, loc.getYaw(),
-                loc.getPitch());
-    }
-
-    public static Vector rotate(float yaw, float pitch, double x, double y,
-            double z)
-    {
-        float angle = yaw * 0.01745329F;
-        double sinyaw = Math.sin(angle);
-        double cosyaw = Math.cos(angle);
-
-        angle = pitch * 0.01745329F;
-        double sinpitch = Math.sin(angle);
-        double cospitch = Math.cos(angle);
-
-        double newx = 0.0D;
-        double newy = 0.0D;
-        double newz = 0.0D;
-        newz -= x * cosyaw;
-        newz -= y * sinyaw * sinpitch;
-        newz -= z * sinyaw * cospitch;
-        newx += x * sinyaw;
-        newx -= y * cosyaw * sinpitch;
-        newx -= z * cosyaw * cospitch;
-        newy += y * cospitch;
-        newy -= z * sinpitch;
-
-        return new Vector(newx, newy, newz);
-    }
+public class Functions{
 
     public static boolean debuffs(PotionEffectType pet)
     {
@@ -84,7 +34,8 @@ public class Functions
             return false;
         }
     }
-
+    
+    // NEEDS TO BE MOVED
     public static int abilityCap(double maxAmount, double currentLevel)
     {
         int tempCap = (int) (maxAmount * (currentLevel / getBaseMaxLevel()));
@@ -97,6 +48,7 @@ public class Functions
         return tempCap;
     }
 
+    // NEEDS TO BE MOVED
     public static int averagePlayerLevel()
     {
         @SuppressWarnings("deprecation")
@@ -115,6 +67,7 @@ public class Functions
         return AverageLevel;
     }
 
+    // NEEDS TO BE MOVED
     public static int maxPlayerLevel()
     {
         @SuppressWarnings("deprecation")
@@ -133,6 +86,7 @@ public class Functions
         return maxLevel;
     }
 
+    // NEEDS TO BE MOVED
     public static int getBaseMaxLevel()
     {
         int tempBase = (int) Math.round(averagePlayerLevel() * 1.2);
@@ -143,6 +97,20 @@ public class Functions
     public static int getMinMaxLevel()
     {
         return 60;
+    }
+    
+    // NEEDS TO BE MOVED
+    public static double ScaleDamage(int Level, double damageDone,
+            int levelDivider)
+    {
+        return damageDone + Level / levelDivider;
+    }
+
+    // NEEDS TO BE MOVED
+    public static double damageToDo(double damageDone, double currentHealth, double maxHealth){
+        return damageDone
+                + (damageDone * (1 - EntityFunctions
+                        .entityCurrentHealthPercent(currentHealth, maxHealth)));
     }
 
     @SuppressWarnings("deprecation")
@@ -163,97 +131,11 @@ public class Functions
         p.updateInventory();
     }
 
-    public static Player[] getNearbyPlayers(Location l, int radius)
-    {
-        int chunkRadius = radius < 16 ? 1 : (radius - (radius % 16)) / 16;
-        HashSet<Entity> radiusEntities = new HashSet<Entity>();
-
-        for (int chX = 0 - chunkRadius; chX <= chunkRadius; chX++)
-        {
-            for (int chZ = 0 - chunkRadius; chZ <= chunkRadius; chZ++)
-            {
-                int x = (int) l.getX(), y = (int) l.getY(), z = (int) l.getZ();
-
-                for (Entity e : new Location(l.getWorld(), x + (chX * 16), y, z
-                        + (chZ * 16)).getChunk().getEntities())
-                {
-                    if (e instanceof Player
-                            && e.getLocation().distance(l) <= radius)
-                    {
-                        radiusEntities.add((Player) e);
-                    }
-                }
-            }
-        }
-
-        return radiusEntities.toArray(new Player[radiusEntities.size()]);
-    }
-
-    public static LivingEntity GetDamager(EntityDamageByEntityEvent event)
-    {
-        Entity damagerEntity = event.getDamager();
-
-        if (damagerEntity.getType() == EntityType.PLAYER)
-        {
-            return (LivingEntity) damagerEntity;
-        }
-        else if (damagerEntity.getType() == EntityType.ARROW)
-        {
-            // Arrow currentArrow = (Arrow) damagerEntity;
-            // return currentArrow.getShooter();
-            return null;
-        }
-        else if (damagerEntity.getType() == EntityType.FIREBALL)
-        {
-            return null;
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    @SuppressWarnings("deprecation")
-    public static Player[] getNearbyPlayersNew(Location l, int radius)
-    {
-        HashSet<Entity> radiusEntities = new HashSet<Entity>();
-
-        for (Player p : Bukkit.getServer().getOnlinePlayers())
-        {
-            if (p.getLocation().getWorld() == l.getWorld()
-                    && p.getLocation().distance(l) <= radius)
-            {
-                radiusEntities.add(p);
-            }
-        }
-
-        return radiusEntities.toArray(new Player[radiusEntities.size()]);
-    }
-
     public static Location offsetLocation(Location l, double x, double y,
             double z)
     {
         return new Location(l.getWorld(), l.getX() + x, l.getY() + y, l.getZ()
                 + z);
-    }
-
-    public static double ScaleDamage(int Level, double damageDone,
-            int levelDivider)
-    {
-        return damageDone + Level / levelDivider;
-    }
-
-    public static double damageToDo(double damageDone, double currentHealth,
-            double maxHealth)
-    {
-        return damageDone
-                + (damageDone * (1 - EntityFunctions
-                        .entityCurrentHealthPercent(currentHealth, maxHealth)));
-    }
-
-    public static boolean isNumberBetween(int numberToCheck, int low, int high)
-    {
-        return (numberToCheck >= low && numberToCheck < high);
     }
 
     public static boolean deosPlayersInventoryContainAtLeast(Player p,
@@ -269,116 +151,8 @@ public class Functions
                 new ItemStack(new Potion(pt, level).toItemStack(1)), amountInI);
     }
 
-    public static boolean numberBetween(double number, double start, double end)
-    {
+    public static boolean numberBetween(double number, double start, double end){
         return (number >= start) && (number < end);
-    }
-
-    public static Location lookAt(Location loc, Location lookat)
-    {
-        loc = loc.clone();
-
-        double dx = lookat.getX() - loc.getX();
-        double dy = lookat.getY() - loc.getY();
-        double dz = lookat.getZ() - loc.getZ();
-
-        if (dx != 0)
-        {
-            if (dx < 0)
-            {
-                loc.setYaw((float) (1.5 * Math.PI));
-            }
-            else
-            {
-                loc.setYaw((float) (0.5 * Math.PI));
-            }
-            loc.setYaw((float) loc.getYaw() - (float) Math.atan(dz / dx));
-        }
-        else if (dz < 0)
-        {
-            loc.setYaw((float) Math.PI);
-        }
-
-        double dxz = Math.sqrt(Math.pow(dx, 2) + Math.pow(dz, 2));
-
-        loc.setPitch((float) -Math.atan(dy / dxz));
-        loc.setYaw(-loc.getYaw() * 180f / (float) Math.PI);
-        loc.setPitch(loc.getPitch() * 180f / (float) Math.PI);
-
-        return loc;
-    }
-
-    public static List<Block> getBlocksInRadius(Location l, int radius)
-    {
-    	// Test 123
-        World w = l.getWorld();
-        int xCoord = (int) l.getX();
-        int zCoord = (int) l.getZ();
-        int YCoord = (int) l.getY();
-
-        List<Block> tempList = new ArrayList<Block>();
-
-        for (int x = 0; x <= 2 * radius; x++)
-        {
-            for (int z = 0; z <= 2 * radius; z++)
-            {
-                for (int y = 0; y <= 2 * radius; y++)
-                {
-                    tempList.add(w.getBlockAt(xCoord + x, YCoord + y, zCoord
-                            + z));
-                }
-            }
-        }
-        return tempList;
-    }
-
-    public static void Heal(LivingEntity entityToHeal, int amountToHeal)
-    {
-        Damageable dEntityToHeal = (Damageable) entityToHeal;
-        double max = dEntityToHeal.getMaxHealth();
-        double current = dEntityToHeal.getHealth();
-
-        if (entityToHeal != null)
-        {
-            for (int i = amountToHeal; i != 0; i--)
-            {
-                if ((current + i) < max)
-                {
-                    entityToHeal.setHealth(current + i);
-                }
-            }
-        }
-    }
-
-    public static void RestoreHunger(Player entityToRestore, int amountToRestore)
-    {
-        if (entityToRestore != null)
-        {
-            for (int i = amountToRestore; i != 0; i--)
-            {
-                if ((entityToRestore.getFoodLevel() + i) < 10)
-                {
-                    entityToRestore.setFoodLevel(entityToRestore.getFoodLevel()
-                            + i);
-                }
-            }
-        }
-    }
-
-    public static void RestoreSaturation(Player entityToRestore,
-            int amountToRestore)
-    {
-        if (entityToRestore != null)
-        {
-            for (int i = amountToRestore; i != 0; i--)
-            {
-                if ((entityToRestore.getSaturation() + i) < 10)
-                {
-                    entityToRestore.setSaturation(entityToRestore
-                            .getSaturation() + i);
-                }
-            }
-        }
     }
 
     public static List<Block> getBlocksBetweenPoints(Location min, Location max)
@@ -404,32 +178,4 @@ public class Functions
         return (seconds * 20);
     }
 
-    private final static List<EntityType> mobs = Arrays.asList(
-            EntityType.BLAZE, EntityType.CAVE_SPIDER, EntityType.CREEPER,
-            EntityType.ENDER_DRAGON, EntityType.ENDERMAN, EntityType.GHAST,
-            EntityType.MAGMA_CUBE, EntityType.PIG_ZOMBIE, EntityType.SKELETON,
-            EntityType.SPIDER, EntityType.SLIME, EntityType.WITCH,
-            EntityType.WITHER, EntityType.ZOMBIE);
-
-    public static boolean IsAMob(EntityType currentEntityType)
-    {
-        return mobs.contains(currentEntityType);
-    }
-
-    public static double MobMaxHealth(LivingEntity entity)
-    {
-        // -1 is for slime's
-
-        final double[] maxHP =
-        { 20.0, 12.0, 20.0, 200.0, 40.0, 10.0, -1, 20.0, 20.0, 16.0, -1, 26.0,
-                300.0, 20.0 };
-
-        final double[] slimeSize =
-        { 1.0, 4.0, 16.0 };
-
-        final double mhp = maxHP[mobs.indexOf(entity)];
-
-        return (mhp != -1) ? mhp : slimeSize[(int) (Math.log(((Slime) entity)
-                .getSize()) / Math.log(2))];
-    }
 }
